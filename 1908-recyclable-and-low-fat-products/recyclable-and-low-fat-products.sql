75417b16c8813f972f298429acef4596501827f8
Donia Eid <doniaeid815@gmail.com>
2026-04-10 16:45:08 +0200

    Time: 653 ms (11.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+# first solution
+select product_id 
+from products p
+where p.low_fats =  'Y' and p.recyclable='Y'
+
+# second solution
+-- SELECT product_id 
+-- FROM Products
+-- WHERE low_fats LIKE 'Y%' 
+--   AND recyclable LIKE 'Y%';
+
+
+
