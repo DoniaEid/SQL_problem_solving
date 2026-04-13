66d7193b5d8dfb57759a122049ca682f92c2f499
Donia Eid <doniaeid815@gmail.com>
2026-04-13 17:15:21 +0200

    Time: 510 ms (44.53%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -4,7 +4,7 @@
 
 select project_id ,round(avg(experience_years),2)AS  average_years
  
- from Project P inner join Employee E
+ from Project P left join Employee E
  on P.employee_id=E.employee_id
  group by project_id
 
