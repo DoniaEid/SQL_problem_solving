725e65102cb31a3c84d3eca4d76af25fc7fe905b
Donia Eid <doniaeid815@gmail.com>
2026-04-17 00:51:40 +0200

    Time: 359 ms (30.47%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,20 +1,5 @@
-SELECT name 
-FROM Employee 
-WHERE id IN (
-    SELECT managerId 
-    FROM Employee 
-    GROUP BY managerId 
-    HAVING COUNT(id) >= 5
-);
-
-
--- WITH HeavyManagers AS (
---     SELECT managerId
---     FROM Employee
---     GROUP BY managerId
---     HAVING COUNT(*) >= 5
--- )
-
--- SELECT E.name
--- FROM Employee E
--- JOIN HeavyManagers HM ON E.id = HM.managerId;
+SELECT m.name
+FROM  Employee e JOIN  Employee m 
+ON e.managerId = m.id
+GROUP BY m.id, m.name
+HAVING  COUNT(e.id) >= 5;
