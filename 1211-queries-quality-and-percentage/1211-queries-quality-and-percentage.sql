e112e13d48f41cd7da4bb54ee04ebe4532747049
Donia Eid <doniaeid815@gmail.com>
2026-04-20 20:25:35 +0200

    Time: 376 ms (28.84%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -23,33 +23,6 @@ on quality .query_name = poor_query .query_name
 
 
 
--- WITH poor_query AS (
---     SELECT 
---         query_name, 
---         COUNT(*) * 100.0 / (SELECT COUNT(*) FROM Queries Q2 WHERE Q2.query_name = Q1.query_name) AS poor_query_percentage
---     FROM Queries Q1
---     WHERE rating < 3
---     GROUP BY query_name
--- ), 
--- quality AS (
---     SELECT 
---         query_name,
---        round(SUM(rating / position) / COUNT(*),2) AS quality_value
---     FROM Queries
---     GROUP BY query_name
--- )
-
--- SELECT 
---     q.query_name, 
---     q.quality_value,
---     p.poor_query_percentage
--- FROM quality q 
--- INNER JOIN poor_query p ON q.query_name = p.query_name;
-
-
-
-
-
 
 
 
