9ba694d1050d01b23d24b841374a636ab5b110e0
Donia Eid <doniaeid815@gmail.com>
2026-04-20 21:03:28 +0200

    Time: 272 ms (16.38%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
 with  processing As(
-     select  machine_id, round( sum(case when activity_type='start'  then -timestamp else  timestamp  end ) /(count(*)/2.0)  ,3) As  processing_time
+     select  machine_id, round( sum(case when activity_type='start'  then -timestamp else  timestamp  end ) /(count(*)/2)  ,3) As  processing_time
      from Activity
      group by  machine_id 
 
