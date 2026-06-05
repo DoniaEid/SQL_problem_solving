7bd24b6a8354de1bae33b162c90e6d534ec18ba9
Donia Eid <doniaeid815@gmail.com>
2026-06-05 19:09:50 +0300

    Time: 580 ms (53.64%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 select customer_id  
-from Customer c inner join product p
-on c.product_key=p.product_key
+from Customer c
 group by  customer_id  
 having count(DISTINCT c.product_key)=(select count(*) from product )
 
