53142c7adc2fbc509499e194dc470a71b540d401
Donia Eid <doniaeid815@gmail.com>
2026-06-17 14:11:26 +0300

    Time: 771 ms (15.02%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,7 +1,3 @@
-
-
-
-
 select  E1.employee_id ,E1. name , count(E2.reports_to) as reports_count ,round(avg(E2.age)) as average_age
 from Employees E1 inner join Employees E2
 on E1.employee_id=E2.reports_to
