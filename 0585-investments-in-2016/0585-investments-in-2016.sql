4e69769e6b21e230507ac076c16427c816fd1fc5
Donia Eid <doniaeid815@gmail.com>
2026-06-17 21:10:17 +0300

    Time: 892 ms (5.03%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -9,17 +9,3 @@ AND
      FROM Insurance i3
      WHERE i3.lat = i.lat AND i3.lon = i.lon) = 1;
 
-     
--- SELECT SUM(i.tiv_2016) AS tiv_2016
--- FROM Insurance i
--- WHERE 
---     (SELECT COUNT(*)
---      FROM Insurance i2
---      WHERE i2.tiv_2015 = i.tiv_2015) > 1
--- AND
---     (SELECT COUNT(*)
---      FROM Insurance i3
---      WHERE i3.lat = i.lat AND i3.lon = i.lon) = 1;
-
-
-
