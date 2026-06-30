655f824f17a01ed5e3ed3a200e5e4a69b990e73b
Donia Eid <doniaeid815@gmail.com>
2026-06-30 16:45:45 +0300

    Time: 1052 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,8 +1,10 @@
-# Write your MySQL query statement below
 
 select  E.employee_id ,E.department_id 
 from Employee E
-where E.primary_flag ='Y' or (E.primary_flag ='N'and (select count(department_id) from Employee Em where E.employee_id=Em.employee_id)=1)
+where E.primary_flag ='Y' or 
+(E.primary_flag ='N' and 
+(select count(department_id) from Employee Em where E.employee_id=Em.employee_id)=1
+)
 
 
 
