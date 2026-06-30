dff2f4d09b39720a161af254f7dcd5208721a96d
Donia Eid <doniaeid815@gmail.com>
2026-06-30 14:31:09 +0300

    Time: 287 ms (52.56%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,13 @@
+SELECT IFNULL(
+    (
+        SELECT DISTINCT salary
+        FROM Employee
+        WHERE salary !=(
+            SELECT MAX(salary)
+            FROM Employee
+        )
+        ORDER BY salary DESC
+        LIMIT 1
+    ),
+    NULL
+) AS SecondHighestSalary;
