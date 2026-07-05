5b02952a43cb7ae4a4a90c515a367568938d2063
Donia Eid <doniaeid815@gmail.com>
2026-07-05 19:46:12 +0300

    Time: 654 ms (55.51%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,33 +1,13 @@
-
-
-WITH immediate AS (
-    SELECT d2.customer_id
-    FROM Delivery d1 
-    INNER JOIN Delivery d2 ON d1.customer_id = d2.customer_id 
-    WHERE d1.order_date = d1.customer_pref_delivery_date 
-      AND d1.order_date = (SELECT MIN(c.order_date) FROM Delivery c WHERE c.customer_id = d1.customer_id) 
-    GROUP BY d2.customer_id 
-),
-
-scheduled AS (
-    SELECT d2.customer_id
-    FROM Delivery d1 
-    INNER JOIN Delivery d2 ON d1.customer_id = d2.customer_id 
-    WHERE d1.order_date != d1.customer_pref_delivery_date
-      AND d1.order_date = (SELECT MIN(c.order_date) FROM Delivery c WHERE c.customer_id = d1.customer_id) 
-    GROUP BY d2.customer_id 
-),
-
-count_immediate AS (
-    SELECT COUNT(*) AS i FROM immediate
-),
-count_scheduled AS (
-    SELECT COUNT(*) AS s FROM scheduled
-)
-
 SELECT 
-    ROUND((i / (i + s)) * 100, 2) AS immediate_percentage
-FROM count_immediate, count_scheduled;
+    ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100, 2) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
+
+
 
 
 
